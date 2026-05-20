3dc31181a1673291af0235ef6466cb766ed29952
Nikolaos Zerzelidis <nickzerze@gmail.com>
2026-05-20 14:16:19 +0300

    Refactor dependencyCheck and publisher calls
    Refactor dependencyCheck call for better readability and maintainability.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,7 +1,9 @@
 def call() {
     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
-        dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY}", odcInstallation: 'OWASP'
+        dependencyCheck(
+            additionalArguments: '--scan ./ --nvdApiKey $NVD_API_KEY',
+            odcInstallation: 'OWASP'
+        )
+        dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
     }
-
-    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
